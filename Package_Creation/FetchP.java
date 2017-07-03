1ad820f15b65151c0154f5741cb01e748971f7c8
RishiPy <royalpandey42@gmail.com>
2017-07-03 23:10:39 +0530

    .

---
+++ Package_Creation/FetchP.java
@@ -0,0 +1,10 @@
+package p2;
+import p1.*;
+class FetchP
+{
+public static void main(String args[])
+{
+SimplePr sp=new SimplePr();
+sp.add(20,30);
+}
+}
---
+++ Package_Creation/SimplePr.java
@@ -0,0 +1,9 @@
+package p1;
+public class SimplePr{
+public void add(int a,int b)
+{
+  int c=a+b;
+  System.out.println("Sum is :"+c);
+
+}
+}
