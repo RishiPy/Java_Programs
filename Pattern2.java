8c9c781c740edd9705f06fe3ff6986b9ea2e475b
RishiPy <royalpandey42@gmail.com>
2017-07-08 14:38:44 +0530

    .

---
+++ Pattern2.java
@@ -0,0 +1,18 @@
+public class Pattern2 {
+
+   
+public static void main(String [] args)
+{
+          int  rows=5;
+        for(int i=1;i<=rows;i++)  //Works as ..how much                                                         line you want to print
+        {
+            for(int j=5;j>=i;j--)
+            {
+                System.out.print(j);
+            }
+System.out.println("");
+  
+        }
+}
+} 
+
